a0d637f5b5a439f5ff1d199faa07f39e46edc911
Prasath Basuvaraj <prasath.buddie@gmail.com>
2024-08-31 09:00:42 -0400

    Skeleton built with Spring Initializer
    Signed-off-by: Prasath Basuvaraj <prasath.buddie@gmail.com>

---
+++ src/test/java/org/hyppar/bucket_list/BucketListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyppar.bucket_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
